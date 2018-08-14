0692934f5d3757b64bdeb752d20d4e699755174b
brianlee <blees.email@gmail.com>
2018-08-14 19:21:08 -0400

    Basic rendering of trips

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,20 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trips: []
+    };
+  }
+
   componentDidMount = async() => {
-    const response = await axios.get('/api/hello');
+    const response = await axios.get('/api/trips');
     try {
       console.log('response is ', response.data)
+      this.setState({
+        trips: response.data.data
+      });
     } catch(e) {
       console.log('error', e)
     }
@@ -19,11 +29,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Subway Trip Planner</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="trips-container">
+          {this.state.trips.map(trip => (
+            <div key={trip.id}>
+              <img src={trip.attributes['route-image-url']} alt={trip.attributes.route}/>
+              Origin-Departure: {trip.attributes['origin-departure']}<br/>
+              Destination: {trip.attributes.destination}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
