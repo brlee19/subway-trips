689f71fd75eac100b6e345beaf7304427ec2946a
brianlee <blees.email@gmail.com>
2018-08-16 11:07:09 -0400

    Proof of concept of multiple parallel calls to API

--- server/index.js
+++ server/index.js
@@ -38,8 +38,36 @@ app.get('/api/trips/:tripId/arrivals', async(req, res) => {
   }
 });
 
+app.get('/api/tripz/all', async(req, res) => {
+  // console.log('i am running')
+  // const pageNumbers = [];
+  // for (let i = 1; i < 51; i++) {
+  //   pageNumbers.push(i);
+  // }
+  // try {
+  //   const queries = pageNumbers.map(i => axios.get(url + `/trips/?pagenumber=${i}`));
+  //   results = await Promise.all(queries);
+  //   console.log('results are', results.map(result => result.data.data));
+  //   console.log('number of results are', results.length)
+  // } catch(e) {
+  //   console.log('error is', e)
+  // }
+
+  const query1 = axios.get("https://nooklyn-interview.herokuapp.com/trips?page%5Bnumber%5D=1&page%5Bsize%5D=20")
+  const query2 = axios.get("https://nooklyn-interview.herokuapp.com/trips?page%5Bnumber%5D=2&page%5Bsize%5D=20")
+  const query3 = axios.get("https://nooklyn-interview.herokuapp.com/trips?page%5Bnumber%5D=3&page%5Bsize%5D=20")
+  const query4 = axios.get("https://nooklyn-interview.herokuapp.com/trips?page%5Bnumber%5D=4&page%5Bsize%5D=20")
+  const query5 = axios.get("https://nooklyn-interview.herokuapp.com/trips?page%5Bnumber%5D=5&page%5Bsize%5D=20")
+  const query6 = axios.get("https://nooklyn-interview.herokuapp.com/trips?page%5Bnumber%5D=6&page%5Bsize%5D=20")
+
+  const results = await Promise.all([query1, query2, query3, query4, query5, query6]);
+  console.log(results.map(result => result.data.data.map(data => data.id)))
+
+  res.send('ok')
+})
+
 app.get('/api/six', async (req, res) => {
-  // get all trips for the 6 train
+  // TEST get all trips for the 6 train
   const results = await axios.get(url + '/trips', {
     params: {
       'filter[route]': 6
