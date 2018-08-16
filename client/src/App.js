c0958447f9ca7d4785a569dfd60625ae30ee52bc
brianlee <blees.email@gmail.com>
2018-08-16 09:32:44 -0400

    Reorganize app state for easier filterability

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,20 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      trips: [],
-      selectedTripId: '',
-      favorites: {
-        lines: [],
-        trips: []
-      },
       arrivals: [],
-      mapCenter: {
+      trips : {
+        all: [],
+        selected: '',
+        favorites: [],
+        visible: []
+      },
+      lines: {
+        all: [],
+        favorites: [],
+        visible: []
+      },
+      visibility: 'all', // can also be 'favoriteTrips' or 'favoriteLines' 
+      mapCenter: { // remove if unable to make map center dynamic
         lat: 40.7128,
         lng: -74.0060
       },
@@ -31,11 +37,22 @@ class App extends Component {
   }
 
   async componentDidMount() {
-    // move to own function in case it needs to be called indepdently
+    // keep here because this call should only be made once per app lifecycle
     try {
       const response = await axios.get('/api/trips');
+      const uniqueLines = [...new Set(response.data.data.map(trip => trip.attributes.route))];
+      console.log('uniqueLines are', uniqueLines)
       this.setState({
-        trips: response.data.data
+        trips: {
+          ...this.state.trips,
+          all: response.data.data,
+          visible: response.data.data.map(trip => trip.id)
+        },
+        lines: {
+          ...this.state.lines,
+          all: [...uniqueLines],
+          visible: [...uniqueLines]
+        }
       });
     } catch(e) {
       console.error('error getting all trips', e)
@@ -43,13 +60,16 @@ class App extends Component {
   }
 
   async selectTrip(id) {
-    if (id === this.state.selectedTripId) return;
+    if (id === this.state.trips.selected) return;
 
     try {
       const response = await axios.get(`/api/trips/${id}/arrivals`);
       console.log('arrivals are', response.data.data)
       this.setState({
-        selectedTripId: id,
+        trips: {
+          ...this.state.trips,
+          selected: id
+        },
         arrivals: response.data.data,
         mapCenter: calculateCentralCoordinates(response.data.data)
       });
@@ -60,13 +80,13 @@ class App extends Component {
 
   addLineToFavorites(trip) {
     const line = trip.attributes.route;
-    if (this.state.favorites.lines.includes(line)) return;
+    if (this.state.lines.favorites.includes(line)) return;
 
     this.setState({
-      favorites: {
-        ...this.state.favorites,
-        lines: [
-          ...this.state.favorites.lines,
+      lines: {
+        ...this.state.lines,
+        favorites: [
+          ...this.state.lines.favorites,
           line
         ]
       }
@@ -75,25 +95,27 @@ class App extends Component {
 
   removeLineFromFavorites(trip) {
     const line = trip.attributes.route;
-    if (!this.state.favorites.lines.includes(line)) return;
+    if (!this.state.lines.favorites.includes(line)) return;
 
     this.setState({
-      favorites: {
-        ...this.state.favorites,
-        lines: this.state.favorites.lines.filter(favoriteLine => favoriteLine !== line)
+      lines: {
+        ...this.state.lines,
+        favorites: [
+          this.state.lines.favorites.filter(favLine => favLine !== line)
+        ]
       }
     });
   }
 
   addTripToFavorites(trip) {
     const tripId = trip.id;
-    if (this.state.favorites.trips.includes(tripId)) return;
+    if (this.state.trips.favorites.includes(tripId)) return;
 
     this.setState({
-      favorites: {
-        ...this.state.favorites,
-        trips: [
-          ...this.state.favorites.trips,
+      trips: {
+        ...this.state.trips,
+        favorites: [
+          ...this.state.trips.favorites,
           tripId
         ]
       }
@@ -102,12 +124,12 @@ class App extends Component {
 
   removeTripFromFavorites(trip) {
     const tripId = trip.id;
-    if (!this.state.favorites.trips.includes(tripId)) return;
+    if (!this.state.trips.favorites.includes(tripId)) return;
     
     this.setState({
-      favorites: {
-        ...this.state.favorites,
-        trips: this.state.favorites.trips.filter(trip => trip !== tripId)
+      trips: {
+        ...this.state.trips,
+        favorites: this.state.trips.favorites.filter(faveTrip => faveTrip !== tripId)
       }
     });
   }
@@ -118,21 +140,23 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Subway Trip Planner</h1>
         </header>
+
         <div className="trips-container">
-          {this.state.trips.map(trip => (
+          {this.state.trips.all.filter(trip => this.state.trips.visible.includes(trip.id))
+                               .map(trip => (
             <Trip key={trip.id}
                   trip={trip}
                   selectTrip={this.selectTrip}
                   isFavorite={{
-                    line: this.state.favorites.lines.includes(trip.attributes.route),
-                    trip: this.state.favorites.trips.includes(trip.id)
+                    line: this.state.lines.favorites.includes(trip.attributes.route),
+                    trip: this.state.trips.favorites.includes(trip.id)
                   }}
                   toggleLineFromFavorites={
-                    this.state.favorites.lines.includes(trip.attributes.route) ?
+                    this.state.lines.favorites.includes(trip.attributes.route) ?
                     this.removeLineFromFavorites : this.addLineToFavorites 
                   }
                   toggleTripFromFavorites={
-                    this.state.favorites.trips.includes(trip.id) ?
+                    this.state.trips.favorites.includes(trip.id) ?
                     this.removeTripFromFavorites : this.addTripToFavorites
                   }
 
